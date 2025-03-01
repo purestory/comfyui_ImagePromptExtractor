55965a9477bff0c8a553775a3fe0f15a32d0d4bf
purestory <purestory@gmail.com>
2025-03-01 13:12:22 +0900

    이미지 프롬프트 추출기 코드 업데이트

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# ComfyUI 노드 로더를 위한 초기화 파일
+from .nodes.image_prompt_extractor import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# 노드 클래스와 디스플레이 이름 매핑을 내보냅니다
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
